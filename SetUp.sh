13f57bb31e4a95476d4c7c59edb903ca4b35a6f7
jonathag <TheAppGardner@gmail.com>
2019-02-25 15:44:29 -0500

    Added gitconfig

--- SetUp.sh
+++ SetUp.sh
@@ -3,10 +3,13 @@
 cd "$( dirname "$0" )"
 SCRIPT_DIR="$( dirs -c; dirs )"
 MY_BASH="$SCRIPT_DIR/.jonathon_bash_profile"
+FULL_PATH="$(pwd)"
+MY_GIT_C="$FULL_PATH/.gitconfig"
 
 if [ -f $(eval echo $MY_BASH) ]; then
   echo "" >> ~/.bashrc
   echo "source $MY_BASH" >> ~/.bashrc
+  cp $MY_GIT_C ~/.gitconfig
 else
   echo "ERROR: Could not find bash ($MY_BASH)."
 fi
