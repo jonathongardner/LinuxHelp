ff44555eb74d195ffe0c1059990735faa4ccf5ef
Jonathon Gardner <theAppGardner@gmail.com>
2024-09-28 14:42:52 -0400

    feat: change command name and add release

--- setup/main.go
+++ setup/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	_ "embed"
+
 	"github.com/jonathongardner/linuxhelp/setup/cli"
 
 	log "github.com/sirupsen/logrus"
 )
 
+//go:embed version.txt
+var version string
+
 func main() {
-	err := cli.Run()
+	err := cli.Run(version)
 	if err != nil {
 		log.Fatal(err)
 	}
